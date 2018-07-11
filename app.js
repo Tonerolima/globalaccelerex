33ca79a1ab95691af6acb1532f9a3872fe5ed47f
Anthony Oyathelemhi <tonero91@gmail.com>
2018-07-11 14:58:40 +0000

    use mongodb database to  store/retrieve users

--- app.js
+++ app.js
@@ -2,61 +2,82 @@ const express = require('express');
 const app = express();
 const faker = require('faker');
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
+// Mongoose connection
+mongoose.connect(process.env.DATABASEURL);
+
+// User Schema setup
+const UserSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    photo: String,
+    photoThumb: String,
+    description: String,
+    nationality: String,
+})
+
+const User = mongoose.model('User', UserSchema);
+
+// Set body parser middleware to use the urlencoded option
 app.use(bodyParser.urlencoded({extended: true}));
 
-let persons = [];
 
-for(let i=0; i<10; i++){
-    let obj = {};
-    obj.firstname = faker.name.firstName();
-    obj.lastname = faker.name.lastName();
-    obj.username = obj.firstname + '.' + obj.lastname;
-    obj.email = faker.internet.email();
-    obj.photo = faker.image.people();
-    obj.photoThumb = faker.image.avatar();
-    obj.description = faker.random.words();
-    obj.nationality = faker.address.country();
+// SEED DB
+// for(let i=0; i<10; i++){
+//     let obj = {};
+//     obj.name = faker.name.findName();
+//     obj.email = faker.internet.email();
+//     obj.photo = faker.image.people();
+//     obj.photoThumb = faker.image.avatar();
+//     obj.description = faker.random.words();
+//     obj.nationality = faker.address.country();
     
-    persons.push(obj);
-}
+//     User.create(obj, (err, user) => {
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log(user);
+//         }
+//     })
+// }
 
+// ROUTES
 app.get('/persons', (req, res) => {
-    return res.status(200).send({status: true, persons: persons});
-})
-
-app.get('/persons/:username', (req, res) => {
-    let newPerson;
-    persons.forEach((person) => {
-        if(req.params.username === person.username){
-            newPerson = person;
+    User.find((err, users) => {
+        if(err){
+            console.log(err);
+            return res.send('Server error, this is being investigated');
         }
+        return res.status(200).send({status: true, persons: users});
     })
-    
-    if(newPerson){
-        return res.status(200).send({status: true, person: newPerson});
-    } else {
-        return res.status(400).send({status: false, message: "Person with username " + req.params.username + " does not exist"});
-    }
+})
+
+app.get('/persons/:id', (req, res) => {
+    User.findById(req.params.id, (err, user) => {
+        if(err){
+            console.log(err);
+            return res.status(400).send({status: false, message: "Person with id " + req.params.id + " does not exist"});
+        }
+        return res.status(200).send({status: true, person: user});
+    })
 })
 
 
-app.post('/persons/:username', (req, res) => {
+app.post('/persons/:id', (req, res) => {
     if(!req.body.description){
         return res.status(400).send({status: false, message: 'Incomplete parameters passed. Missing fields: description'});
     }
-
-    persons.map((person) => {
-        let newPerson = person;
-        if(req.params.username === newPerson.username){
-           newPerson.description = req.body.description;
-           return newPerson;
+    User.findByIdAndUpdate({description: req.body.description}, (err, user) => {
+        if(err){
+            console.log(err);
+            return res.status(400).send({status: false, message: "Person with id " + req.params.id + " does not exist"});
         }
+        res.redirect('/persons/'+req.params.id);  
     })
-    res.redirect('/persons/'+req.params.username);
-    
 })
 
+// SERVER
 app.listen(process.env.PORT, process.env.IP, () => {
     console.log("App is ready");
 })
